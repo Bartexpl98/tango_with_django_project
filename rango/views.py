54e0f65cec17591eab3b1f02b467473363f8f407
Bartosz Zmuda <2576674z@student.gla.ac.uk>
2024-02-07 15:56:33 +0000

    Chapter 8 finished

--- rango/views.py
+++ rango/views.py
@@ -23,6 +23,7 @@ def index(request):
 
 def about(request):
 
+    
     context_dict = {'boldmessage': 'This tutorial has been put together by Bartek'}
 
     return render(request, "rango/about.html",context=context_dict)
